1e8bcaaeaeb9a179da6cec81885dbe4d52d56e52
IMHOSUNG <dlaghtjd4487@naver.com>
2019-10-26 20:48:20 +0900

    ENH : 시계열 데이터 처리 완료

--- server/rpctest/rpcfunction.py
+++ server/rpctest/rpcfunction.py
@@ -1,7 +1,12 @@
 # pip install pika
 
 import pika
-
+import json
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.seasonal import seasonal_decompose
+pd.set_option("display.max_row", 500)
+pd.set_option("display.max_columns", 500)
 userid = "root";
 password = "root";
 
@@ -14,15 +19,40 @@ channel = connection.channel()
 
 channel.queue_declare(queue='rpc_queue')
 
-def fib(n):
-    return n
+def decomposeFun(n):
+    #print(type(n));
+    #print(n);
+    #jsonString = json.dumps(n);
+    #print(type(jsonString));
+    #data = json.loads(jsonString);
+    #s = data.encode('ascii');     
+    #print(str(data));
+    #data = data[1:]
 
+    df = pd.read_json(n);
+
+    decomposition = seasonal_decompose(df.value, extrapolate_trend='freq', freq=30);
+    #print(decomposition.resid[1]);
+    #print(decomposition.trend)
+    #print(decomposition.seasonal)
+
+    resid_data = [];
+    trend_data = [];
+    seasonal_data = [];
+
+    for element in decomposition.resid:
+        resid_data.append(element);
+    for element in decomposition.trend:
+        trend_data.append(element);
+    for element in decomposition.seasonal:
+        seasonal_data.append(element);
+    return str({ 'len' : len(resid_data), 'resid' :resid_data , 'trend' : trend_data , 'seasonal' : seasonal_data });
 
 def on_request(ch, method, props, body):
-    n = str(body)
-
-    print(" [.] fib(%s)" % n)
-    response = fib(n)
+    n = body.decode('utf-8');
+    print("get data from server");
+    #print(" [.] fib(%s)" % n)
+    response = decomposeFun(n)
 
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
