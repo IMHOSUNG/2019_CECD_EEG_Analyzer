4fd02e00904d131df4c9c25f0e5ce493a3a6df63
IMHOSUNG <dlaghtjd4487@naver.com>
2019-11-17 15:51:23 +0900

    ENH : CreatePool 로 변경

--- server/db/connectSet.js
+++ server/db/connectSet.js
@@ -1,4 +1,6 @@
 import mysql from 'mysql'
 import { mysqlConfig } from './config'
 
-export const setDBConnect = mysql.createConnection(mysqlConfig);
+// 단일 커넥션은 시간이 지나면 protocol_connection_lost 문제 발생
+// pool을 사용하면 사용에 있어서 장점이 더 많음.
+export const setDBConnect = mysql.createPool(mysqlConfig);
